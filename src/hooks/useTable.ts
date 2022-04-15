8f9850d20d8968426852855048ef9175b2ab049d
虎虎 <1078013818@qq.com>
2022-04-15 16:11:04 +0800

    usetable

--- src/hooks/useTable.ts
+++ src/hooks/useTable.ts
@@ -1,7 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { ChangeInfo } from "@douyinfe/semi-ui/lib/es/table";
 import { useLayoutEffect } from "react";
 import useRequest from "./useRequest";
-
+useTable.params = {};
+useTable.setParams = (key: any, v?: any) => { };
+useTable.initPage = {
+    page: 1,
+    page_size: 10
+};
 function useTable<T, P = undefined>(
     promise: (data: any) => Promise<any>,
     option: {
@@ -10,7 +16,7 @@ function useTable<T, P = undefined>(
     },
 ): {
     params: P | undefined;
-    setParams: (key: string, v: any) => void;
+    setParams: (key: string | P, v?: any) => void;
     tableData: T | undefined;
     fetch: () => Promise<void>;
     loading: boolean
@@ -25,6 +31,13 @@ function useTable<T, P = undefined>(
         initParams: option?.initParams,
         start_owner: option?.start_owner,
     })
+    useLayoutEffect(() => {
+        useTable.params = params as any
+    }, [params])
+    useLayoutEffect(() => {
+        fetch();
+        useTable.setParams = setParams as any
+    }, [])
 
     return {
         params,
@@ -35,5 +48,37 @@ function useTable<T, P = undefined>(
     }
 }
 
+// table 自定义search
+useTable.search = function (key: string | Record<string, any>, v?: any) {
+    const origin = useTable.params as any;
+    /** 自定义操作 */
+    const _params: any = {
+        ...origin,
+        page: useTable.initPage,
+        search: {
+            ...origin?.search,
+        }
+    }
+    if (typeof key === 'object') {
+        _params.search = { ...key }
+    } else _params.search[key] = v
+    useTable.setParams(_params);
+}
+
+// table云端排序
+useTable.onTableChange = (e: ChangeInfo<any>) => {
+    const { sorter } = e;
+    if (sorter) {
+        const { sortOrder, dataIndex } = sorter;
+        const sortRet: any = {}
+        sortRet[dataIndex] = sortOrder ? `${sortOrder}`?.replace('end', '') : '';
+        useTable.setParams({
+            ...useTable.params,
+            page: useTable.initPage,
+            sort: sortRet
+        })
+        return sortOrder
+    }
+};
 
 export default useTable;
