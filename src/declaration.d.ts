158004bf9852f52faa824a7e54d109eb348ea0a2
虎虎 <1078013818@qq.com>
2022-05-10 15:02:35 +0800

    瀑布流布局

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -3,5 +3,5 @@ declare module 'buffer-layout';
 declare module '@project-serum/serum/lib/token-instructions';
 declare module '@project-serum/serum/lib/market';
 declare module 'bs58';
-
+declare module 'react-responsive-masonry'
 declare module 'sha1'
--- src/pages/main/demo/demo.tsx
+++ src/pages/main/demo/demo.tsx
@@ -1,10 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import ProEchart from "@/components/echart/pro_echart";
-import { memo, useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import './index.scss'
 import { MoveBox, MoveSquare, MoveTigger } from "./movepophover";
 import OssUpload from "./oss/oss_upload";
-
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+const images = [
+    "https://picsum.photos/200/300?image=1050",
+    //...
+    "https://picsum.photos/300/300?image=206",
+]
+class MyWrapper extends React.Component {
+    render() {
+        return (
+            // <ResponsiveMasonry
+            //     columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+            // >
+            <Masonry>
+                <div style={{ height: '100px', background: 'pink', margin: '10px' }}>1</div>
+                <div style={{ height: '120px', background: 'pink', margin: '10px' }}>2</div>
+                <div style={{ height: '140px', background: 'pink', margin: '10px' }}>3</div>
+                <div style={{ height: '90px', background: 'pink', margin: '10px' }}>4</div>
+                <div style={{ height: '160px', background: 'pink', margin: '10px' }}>4</div>
+            </Masonry>
+            // </ResponsiveMasonry>
+        )
+    }
+}
 const arr = [1, 2, 3, 4]
 const DemoComponent = memo(() => {
     const [index, setIndex] = useState(0);
@@ -14,6 +36,8 @@ const DemoComponent = memo(() => {
     }
 
     return <div>
+        <MyWrapper />
+
         <div style={{ width: '100px' }}>
             <OssUpload />
             {/* 层叠布局 */}
@@ -34,6 +58,9 @@ const DemoComponent = memo(() => {
                 </MoveTigger>
                 <MoveSquare>1923890</MoveSquare>
             </MoveBox>
+
+
+
             <ProEchart
                 classname='test'
                 option={{
